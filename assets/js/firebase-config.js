8a5614e056d5a68797c8c887df8c7fea0a83183c
Leonardo Rodrigues de Oliveira <85530968+oliverlleo@users.noreply.github.com>
2026-08-08 17:45:32 -0300

    Add Firebase app configuration for CMS

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,23 @@
+import { initializeApp, getApps } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js';
+import { getAuth, setPersistence, browserLocalPersistence } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-storage.js';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDSr_8pZ99SL5qbZz38guDnIaQ1780Cxrw',
+  authDomain: 'projectazo-9ed9d.firebaseapp.com',
+  projectId: 'projectazo-9ed9d',
+  storageBucket: 'projectazo-9ed9d.firebasestorage.app',
+  messagingSenderId: '265937179992',
+  appId: '1:265937179992:web:432a13556c1f991a65c12d',
+  measurementId: 'G-Q71SM637B4'
+};
+
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+setPersistence(auth, browserLocalPersistence).catch(() => {});
+
+export { app, auth, db, storage, firebaseConfig };
