8ae5215863fb59eabca0921ee0c2e1f9a1b4092f
Leonardo Rodrigues de Oliveira <85530968+oliverlleo@users.noreply.github.com>
2026-08-08 22:27:06 -0300

    Force latest grouped visual editor version

--- admin/visual-editor.js
+++ admin/visual-editor.js
@@ -1,4 +1,4 @@
 // Editor visual atual.
 // A V3 permite escolher todos os textos e imagens de um mesmo bloco,
 // inclusive imagens ocultas de carrosséis.
-import './visual-editor-v3.js?v=20260808-2220';
+import './visual-editor-v3.js?v=20260808-2226';
