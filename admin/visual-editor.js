1604a6fc38a5e2e56b4287d50eb4b64203f27bd4
Leonardo Rodrigues de Oliveira <85530968+oliverlleo@users.noreply.github.com>
2026-08-08 22:28:11 -0300

    Load precise text and stacked-image selector

--- admin/visual-editor.js
+++ admin/visual-editor.js
@@ -1,4 +1,5 @@
 // Editor visual atual.
 // A V3 permite escolher todos os textos e imagens de um mesmo bloco,
 // inclusive imagens ocultas de carrosséis.
-import './visual-editor-v3.js?v=20260808-2226';
+import './visual-editor-v3.js?v=20260808-2232';
+import './visual-editor-v3-precision.js?v=20260808-2232';
